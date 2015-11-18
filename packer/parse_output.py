5c05cce10e9a8e68dfb34e77ca712bfac3945597
James Pancoast <jpancoast@kenzanmedia.com>
2015-11-18 16:29:17 -0700

    now produces both .md and .json ami_table files.

--- packer/parse_output.py
+++ packer/parse_output.py
@@ -5,6 +5,14 @@ import re
 import pprint
 import os
 
+try:
+    import json
+except ImportError, e:
+    print "Missing json module.  Install with: sudo pip install json"
+    print "If you don't have pip, do this first: sudo easy_install pip"
+    exit(2)
+
+
 builders = [ 'hvm_builder' ] #might add pv_builds later... not sure.
 
 md_header="""---
@@ -95,32 +103,11 @@ def main(argv):
 
     md_fo.close()
 
-'''
----
-layout: toc-page
-title: AMI ID table
-id: ami_table
-lang: en
----
+    json_fo = open(artifact_file_location + '/' + artifact_file_json, 'w+')
 
-* Table of contents. This line is required to start the list.
-{:toc}
+    json_fo.write(json.dumps(amis, indent=4, sort_keys=True))
 
-# AMI ID Table
-
-
-| Region         | Name                     | Ubuntu Version | Instance Type | AMI ID       |
-|----------------|--------------------------|----------------|---------------|--------------|
-| us-east-1      | Spinnaker-Ubuntu-14.04-10 | 14.04 LTS      | HVM           | [ami-a3afeac9](https://console.aws.amazon.com/ec2/home?region=us-east-1#launchAmi=ami-a3afeac9) |
-| us-west-1      | Spinnaker-Ubuntu-14.04-10 | 14.04 LTS      | HVM           | [ami-2796f847](https://console.aws.amazon.com/ec2/home?region=us-west-1#launchAmi=ami-2796f847) |
-| us-west-2      | Spinnaker-Ubuntu-14.04-10 | 14.04 LTS      | HVM           | [ami-02766663](https://console.aws.amazon.com/ec2/home?region=us-west-2#launchAmi=ami-02766663) |
-| eu-west-1      | Spinnaker-Ubuntu-14.04-10 | 14.04 LTS      | HVM           | [ami-e30dd790](https://console.aws.amazon.com/ec2/home?region=eu-west-1#launchAmi=ami-e30dd790) |
-| eu-central-1   | Spinnaker-Ubuntu-14.04-10 | 14.04 LTS      | HVM           | [ami-09203265](https://console.aws.amazon.com/ec2/home?region=eu-central-1#launchAmi=ami-09203265) |
-| ap-southeast-1 | Spinnaker-Ubuntu-14.04-10 | 14.04 LTS      | HVM           | [ami-6993520a](https://console.aws.amazon.com/ec2/home?region=ap-southeast-1#launchAmi=ami-6993520a) |
-| ap-southeast-2 | Spinnaker-Ubuntu-14.04-10 | 14.04 LTS      | HVM           | [ami-de520bbd](https://console.aws.amazon.com/ec2/home?region=ap-southeast-2#launchAmi=ami-de520bbd) |
-| ap-northeast-1 | Spinnaker-Ubuntu-14.04-10 | 14.04 LTS      | HVM           | [ami-9c1033f2](https://console.aws.amazon.com/ec2/home?region=ap-northeast-1#launchAmi=ami-9c1033f2) |
-| sa-east-1      | Spinnaker-Ubuntu-14.04-10 | 14.04 LTS      | HVM           | [ami-5d47fd31](https://console.aws.amazon.com/ec2/home?region=sa-east-1#launchAmi=ami-5d47fd31) |
-'''
+    json_fo.close()
 
 
 if __name__ == "__main__":
