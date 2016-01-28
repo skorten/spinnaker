776872d0847570071fd65d6950a81b78d24328f7
Sean Korten <skorten@kenzan.com>
2016-01-28 13:22:57 -0700

    updated scripts and configuration to build both deb and rpm packages

---
+++ pkg_scripts/rpm/postInstall.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# This doesnt belong here, but I cannot figure out where it goes
+# and how other files have it set.
+chmod +x /opt/spinnaker/install/first_google_boot.sh
+
+# Deprecated. Will be removed in the future.
+if [ `readlink -f /opt/spinnaker/scripts` != "/opt/spinnaker/bin" ]; then
+  ln -s /opt/spinnaker/bin /opt/spinnaker/scripts
+fi
+
+<<<<<<< HEAD
+if [ ! -f /opt/spinnaker/config/spinnaker-local.yml ]; then
+  # Create master config on original install, but leave in place on upgrades.
+  cp /opt/spinnaker/config/default-spinnaker-local.yml /opt/spinnaker/config/spinnaker-local.yml
+fi
+=======
+# Create master config
+cp /opt/spinnaker/config/default-spinnaker-local.yml /opt/spinnaker/config/spinnaker-local.yml
+>>>>>>> restructure to support building functional deb and rpm packages
+
+# deck settings
+/opt/spinnaker/bin/reconfigure_spinnaker.sh
+
+# vhosts
+rm -rf /etc/apache2/sites-enabled/*.conf
+
+ln -s /etc/apache2/sites-available/spinnaker.conf /etc/apache2/sites-enabled/spinnaker.conf
+
+sed -i "s/Listen\ 80/Listen 127.0.0.1:9000/" /etc/apache2/ports.conf
+
+service apache2 restart
+
+# Install cassandra keyspaces
+cqlsh -f "/opt/spinnaker/cassandra/create_echo_keyspace.cql"
+cqlsh -f "/opt/spinnaker/cassandra/create_front50_keyspace.cql"
+cqlsh -f "/opt/spinnaker/cassandra/create_rush_keyspace.cql"
+
+# Start all the services
+
+# start 'clouddriver'
+# start 'orca'
+# start 'front50'
+# start 'rush'
+# start 'rosco'
+# start 'echo'
+# start 'gate'
+# start 'igor'
+
+
